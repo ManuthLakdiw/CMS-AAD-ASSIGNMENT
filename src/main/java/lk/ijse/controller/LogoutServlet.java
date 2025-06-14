3ae6a6777d9aaf2c8dcd6ac4fadde4f1b1539df9
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-06-15 00:45:50 +0530

    Add `LogoutServlet.java` to handle user logout with session invalidation and redirection to sign-in page

---
+++ src/main/java/lk/ijse/controller/LogoutServlet.java
@@ -0,0 +1,25 @@
+package lk.ijse.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @author manuthlakdiv
+ * @email manuthlakdiv2006.com
+ * @project CMS
+ * @github https://github.com/ManuthLakdiw
+ */
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
+        resp.sendRedirect(req.getContextPath() + "/view/signin.jsp");
+    }
+}
