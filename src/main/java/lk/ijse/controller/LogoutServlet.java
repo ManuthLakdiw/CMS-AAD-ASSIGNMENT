57dcbf29c1d2db061369ea04f583277a423e83a0
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-06-15 16:14:28 +0530

    Handle null sessions in `LogoutServlet` to prevent potential `NullPointerException`.

--- src/main/java/lk/ijse/controller/LogoutServlet.java
+++ src/main/java/lk/ijse/controller/LogoutServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -19,7 +20,10 @@ import java.io.IOException;
 public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getSession().invalidate();
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
         resp.sendRedirect(req.getContextPath() + "/view/signin.jsp");
     }
 }
