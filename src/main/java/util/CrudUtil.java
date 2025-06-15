86a7a9ee3f468074aca70c50535a0179591313a0
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-06-15 16:11:42 +0530

    Refactor `CrudUtil` to remove static `DataSource` initialization and enhance `execute` method for flexibility with external connections.

--- src/main/java/util/CrudUtil.java
+++ src/main/java/util/CrudUtil.java
@@ -17,36 +17,21 @@ import java.sql.SQLException;
  */
 public class CrudUtil {
 
-//    @Resource(name = "java:comp/env/jdbc/pool")
-    private static final DataSource dataSource;
-
-    static {
-        try {
-            InitialContext context
-                    = new InitialContext();
-            dataSource = (DataSource) context.lookup("java:comp/env/jdbc/pool");
-        } catch (NamingException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    public static <T>T execute(Connection connection, String sql, Object...obj) throws SQLException {
 
 
-    public static <T>T execute(String sql, Object...obj) throws SQLException {
-        Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            for (int i = 0; i < obj.length; i++) {
+                preparedStatement.setObject(i+1,obj[i]);
+            }
 
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        for (int i = 0; i < obj.length; i++) {
-            preparedStatement.setObject(i+1,obj[i]);
-        }
-
-        if(sql.startsWith("SELECT") || sql.startsWith("select")){
-            return (T) preparedStatement.executeQuery();
-        }else {
-            int i = preparedStatement.executeUpdate();
-            boolean isSaved = i > 0;
-            return (T) ((Boolean) isSaved);
-        }
-
+            if(sql.startsWith("SELECT") || sql.startsWith("select")){
+                return (T) preparedStatement.executeQuery();
+            }else {
+                int i = preparedStatement.executeUpdate();
+                boolean isSaved = i > 0;
+                return (T) ((Boolean) isSaved);
+            }
 
     }
 
